2e925e9f2f899450eb29439be61971367f44283d
Eduardo Pozos Huerta <edupozoshuerta@gmail.com>
2025-10-03 16:03:21 -0600

    Firts Commit

---
+++ source/global/movement.gd
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: AGPL-3-or-later
+
+# ************************************
+# *** Author - Pexnídi Game Studio ***
+# *** File   - movement.gd         ***
+# *** Date   - 03/10/2025          ***
+# *** Update - 03/10/2025          ***
+# ************************************
+
+extends Node2D
